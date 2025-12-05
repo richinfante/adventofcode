f9bb861988d10b98d4fc594b86c679c71c523a73
Rich Infante <rich@richinfante.com>
2025-12-04 21:29:25 -0500

    2025 day 2

---
+++ 2025/d2.py
@@ -0,0 +1,34 @@
+def is_valid_id(num_str):
+  is_valid_id = False
+  for i in range(0, len(num_str) // 2):
+    b_char = num_str[i]
+    e_char = num_str[len(num_str) // 2 + i]
+    if b_char != e_char:
+      is_valid_id = True
+      break
+
+  return is_valid_id
+if __name__ == "__main__":
+  input_f = open("2025/d2_input.txt", "r")
+  data = input_f.read().strip().split(',')
+
+  # parse into (a, b) tuples
+  pairs = []
+  for item in data:
+    a, b = item.split('-')
+    pairs.append((int(a), int(b)))
+
+  invalid_sum = 0
+  for pair in pairs:
+    first, second = pair
+
+    for n in range(first, second + 1):
+      num_str = str(n)
+
+      if len(num_str) % 2 != 0:
+        continue
+
+      if not is_valid_id(num_str):
+        invalid_sum += n
+
+  print(invalid_sum)
---
+++ 2025/d2p2.py
@@ -0,0 +1,64 @@
+def is_valid_id(num_str):
+  """
+  can't have any repeats
+  e.g 123123, 121212, 333, 11
+  """
+  min_slices = 2
+  max_slices = len(num_str) + 1
+  for num_repeats in range(min_slices, max_slices):
+    # only want repeat #s that evenly divide the string
+    if len(num_str) % num_repeats != 0:
+      continue
+
+    # split into n_repeats chunks
+    chunks = []
+    chunk_size = len(num_str) // num_repeats
+    for i in range(0, len(num_str), chunk_size):
+      chunks.append(num_str[i:i + chunk_size])
+
+    # check if all are same (compare 1st with rest)
+    all_same = True
+    first_chunk = chunks[0]
+    for chunk in chunks[1:]:
+      if chunk != first_chunk:
+        all_same = False
+        break
+
+    if all_same:
+      return False
+
+  return True
+
+assert not is_valid_id('11')
+assert not is_valid_id('22')
+assert not is_valid_id('999')
+assert not is_valid_id('1010')
+assert not is_valid_id('222222')
+assert not is_valid_id('38593859')
+assert not is_valid_id('38593859')
+assert not is_valid_id('824824824')
+assert not is_valid_id('2121212121')
+assert not is_valid_id('565656')
+assert is_valid_id('837817')
+
+if __name__ == "__main__":
+  input_f = open("2025/d2_input.txt", "r")
+  data = input_f.read().strip().split(',')
+
+  # parse into (a, b) tuples
+  pairs = []
+  for item in data:
+    a, b = item.split('-')
+    pairs.append((int(a), int(b)))
+
+  invalid_sum = 0
+  for pair in pairs:
+    first, second = pair
+
+    for n in range(first, second + 1):
+      num_str = str(n)
+
+      if not is_valid_id(num_str):
+        invalid_sum += n
+
+  print(invalid_sum)
